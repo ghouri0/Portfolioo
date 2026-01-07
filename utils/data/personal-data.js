f4503932a9db9e2adc7e6abaab5c9e4eddd42500
ghouri0 <alimuhammadghouri0@gmail.com>
2026-01-07 11:25:31 +0500

    portfolio chnages - resume added

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -15,5 +15,5 @@ export const personalData = {
   // leetcode: "https://leetcode.com/said3812/",
   devUsername: "muhammadali",
   resume:
-    "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+    "https://drive.google.com/file/d/13TzOwBisaqILlWnQ8EtxxLK4zwUGFCx5/view?usp=sharing",
 };
